b52afd68ed773aa9438ff3c7d7e1088b6869b0e8
loverikim <loverikim03@gmail.com>
2024-05-13 11:22:41 +0900

    향상된 for문

--- src/java_ch05_0513/ArrayTest02.java
+++ src/java_ch05_0513/ArrayTest02.java
@@ -16,7 +16,24 @@ public class ArrayTest02 {
 						 };
 						// 50 -> (1,1) 
 						// 70 -> (2,0)
+		// 향상된 for문
+		// arr1 배열의 모든 원소의 총합을 구하시오.
+		int[] arr1 = {10,20,30,40,50};
+		int sum = 0;
+//		for(int i=0;i<arr1.length;i++){ // i = 0 1 2 3 4
+//			sum += arr1[i];			
+//		}
+//		System.out.println(sum);
 		
+		// 파이썬에서는
+		// for i in arr1:
+		// 	sum = sum +i
+		
+		// 향상된 for문 ---> 새로나운 문법
+		for(int i : arr1) { // in 대신 :사용
+			sum = sum + i;
+		}
+		System.out.println(sum);
 		
 	}
 
