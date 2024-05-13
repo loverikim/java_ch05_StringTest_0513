935887f6db7dca6d40f657a7ca12037180ea700d
loverikim <loverikim03@gmail.com>
2024-05-13 15:07:06 +0900

    열거

---
+++ src/java_ch05_0513/EnumTest.java
@@ -0,0 +1,17 @@
+package java_ch05_0513;
+
+public class EnumTest {
+	//열거 타입 한정된 값들을 가지는 볁수 타입을 사용자 정의로 만들어서 사용
+	public enum Week{MONDAY,TUESDAY, WEDNESDAY,THURSDAY,FRIDAY,SATURDAY,SUNDAY}
+	//첫글자는 대문자로 작명
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		Week today;
+		today = Week.SUNDAY;	
+	}
+	
+	
+	// 클래스 내에 선언된 변수를 속성, 필드, 멤버변수
+	// 클래스 안에 선언된 함수는 메소드 
+		
+}
